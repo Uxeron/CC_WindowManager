fe6c40440bb51f3b003bae9c2c5457e5dd4fb19a
Uxeron <vgabrielius123@gmail.com>
2021-01-10 13:02:18 +0200

    WM now handles (and if needed, retransmits) all mouse events.

--- WindowManager.lua
+++ WindowManager.lua
@@ -1,103 +1,136 @@
-local interface = peripheral.wrap("back")
-local canvas = interface.canvas()
+local glasses = peripheral.wrap("back")
+local canvas = glasses.canvas()
 canvas.clear()
 
 local titlebarHeight = 8  -- Height of the titlebar, same for every window
-local nextWindowID = 0 -- ID for the next created window, constantly incrementing
+local nextWindowID = 1 -- ID for the next created window, constantly incrementing
 
 windows = {} -- Table of windows, stored as ID:Window
-
--- Check if the (x, y) point is within the given rectangle
-function inSquare(x, y, startX, startY, width, height)
-    return ((x > startX and x < startX + width) and (y > startY and y < startY + height))
-end
 
 local clickWithinClose = -1 -- Close happens on release, make sure the press was also on the same close button
 local selected = -1 -- ID of window being dragged
 local lastPosX = 0  -- Mouse x position from the previous drag event
 local lastPosY = 0  -- Mouse y position from the previous drag event
 
+-- Check if the (x, y) point is within the given rectangle
+function inSquare(x, y, startX, startY, width, height)
+    return ((x > startX and x < startX + width) and (y > startY and y < startY + height))
+end
+
+-- Check if click happened in the given window (rectangle)
+function clickedInWindow(clickX, clickY, group, window)
+    local groupX, groupY = group.getPosition()
+
+    local winX, winY = window.getPosition()
+    local winW, winH = window.getSize()
+    return inSquare(clickX, clickY, winX + groupX, winY + groupY, winW, winH)
+end
+
 -- Create a window with the given title, position and size (size does not include the titlebar)
-function addWindow(title, posX, posY, sizeX, sizeY)
+function addWindow(title, id, posX, posY, sizeX, sizeY)
     -- Draw the window
     local group = canvas.addGroup({posX, posY})
+    local programGroup = group.addGroup({0, titlebarHeight})
     local window = group.addRectangle(0, titlebarHeight, sizeX, sizeY, 0x222222FF)
     local titlebar = group.addRectangle(0, 0, sizeX, titlebarHeight, 0x111111FF)
     local title = group.addText({1, 1}, title, 0x444444FF, 0.8)
-    local buttonClose = group.addRectangle(sizeX - titlebarHeight + 1, 1, titlebarHeight - 2, titlebarHeight - 2, 0x111111FF)
-    group.addLine({sizeX - titlebarHeight + 2, 2}, {sizeX - 2, titlebarHeight - 2}, 0x444444FF, 5)
-    group.addLine({sizeX - titlebarHeight + 2, titlebarHeight - 2}, {sizeX - 2, 2}, 0x444444FF, 5)
+
+    -- Draw the close button
+    local closeButtonX = sizeX - titlebarHeight
+    local close = group.addRectangle(closeButtonX, 0, titlebarHeight, titlebarHeight, 0x111111FF)
+    group.addLine({closeButtonX + 2, 2}, {sizeX - 2, titlebarHeight - 2}, 0x444444FF, 5)
+    group.addLine({closeButtonX + 2, titlebarHeight - 2}, {sizeX - 2, 2}, 0x444444FF, 5)
 
     -- Add it to the table of windows
-    windows[nextWindowID] = {
+    windows[id] = {
         Group = group,
+        ProgramGroup = programGroup,
         Window = window,
         Titlebar = titlebar,
         Title = title,
-        ButtonClose = buttonClose
+        Close = close
     }
-
-    -- Increment the ID
-    nextWindowID = nextWindowID + 1
-    return nextWindowID - 1
 end
 
-addWindow("Title", 10, 10, 80, 40)
-addWindow("Title2", 100, 10, 40, 40)
-addWindow("Title3", 10, 60, 40, 80)
+addWindow("Title", 1, 10, 10, 80, 40)
+addWindow("Title2", 2, 100, 10, 40, 40)
+addWindow("Title3", 3, 10, 60, 40, 80)
 
-while true do
-    local name, index, x, y = os.pullEvent()
+-- Try to find if the mouse event happened within any of the open windows and retransmit the event to it
+function retransmitMouseEvent(name, index, x, y, id)
+    id = id or -1 -- Default value
 
-    if name == "glasses_click" and index == 1 then
-        -- Clear the selected value in case glasses_up wasn't caught
+    if id == -1 then
+        for ID, window in pairs(windows) do
+            if clickedInWindow(x, y, window["Group"], window["Window"]) then
+                id = ID
+                return
+            end
+        end
+    end
+    
+    os.queueEvent(name, id, index, x, y)
+end
+
+
+function handleGlassesClick(index, x, y)
+    if index == 1 then
+        -- Clear the values in case glasses_up wasn't caught
+        clickWithinClose = -1
         selected = -1
         lastPosX = 0
         lastPosY = 0
-
-        for ID, window in pairs(windows) do
-            local groupX, groupY = window["Group"].getPosition()
-    
-            local butX, butY = window["ButtonClose"].getPosition()
-            local butW, butH = window["ButtonClose"].getSize()
-            if inSquare(x, y, butX + groupX, butY + groupY, butW, butH) then
-                clickWithinClose = ID
-                break
-            else
-                local titleX, titleY = window["Titlebar"].getPosition()
-                local titleW, titleH = window["Titlebar"].getSize()
-                if inSquare(x, y, titleX + groupX, titleY + groupY, titleW, titleH) then
-                    selected = ID
-                    lastPosX = x
-                    lastPosY = y
-                    break
-                end
-            end
-        end        
-
     end
 
-    if name == "glasses_up" and index == 1 then
-        -- Clear the selected value
-        selected = -1
-        lastPosX = 0
-        lastPosY = 0
+    for ID, window in pairs(windows) do
+        -- Clicked on the X button, mark it for when glasses_up event arrives
+        if index == 1 and clickedInWindow(x, y, window["Group"], window["Close"]) then
+            clickWithinClose = ID
+            return
+        end
 
-        if clickWithinClose ~= -1 then
-            local groupX, groupY = windows[clickWithinClose]["Group"].getPosition()
-
-            local butX, butY = windows[clickWithinClose]["ButtonClose"].getPosition()
-            local butW, butH = windows[clickWithinClose]["ButtonClose"].getSize()
-            if inSquare(x, y, butX + groupX, butY + groupY, butW, butH) then
-                windows[clickWithinClose]["Group"].remove() -- close this window
-                windows[clickWithinClose] = nil
-            end
-
-            clickWithinClose = -1
+        -- Clicked on the titlebar, prepare for dragging
+        if index == 1 and clickedInWindow(x, y, window["Group"], window["Titlebar"]) then
+            selected = ID
+            lastPosX = x
+            lastPosY = y
+            return
         end
     end
 
-    if name == "glasses_drag" and index == 1 and selected ~= -1 then
+    retransmitMouseEvent("wm_glasses_click", index, x, y)
+end
+
+
+function handleGlassesUp(index, x, y)
+    -- Stop dragging
+    if index == 1 and selected ~= -1 then
+        selected = -1
+        lastPosX = 0
+        lastPosY = 0
+        return -- Do not retransmit the release to the programs because the click happened on the titlebar
+    end
+
+    -- Try to check if release happened within the same close button as the click
+    if index == 1 and clickWithinClose ~= -1 then
+        local window = windows[clickWithinClose]
+        if clickedInWindow(x, y, window["Group"], window["Close"]) then
+            os.queueEvent("wm_terminate", clickWithinClose)
+            window["Group"].remove() -- close this window
+            windows[clickWithinClose] = nil
+        end
+
+        clickWithinClose = -1
+        return -- Do not retransmit the release to the programs because the click happened on the close button
+    end
+
+    retransmitMouseEvent("wm_glasses_up", index, x, y)
+end
+
+
+function handleGlassesDrag(index, x, y)
+    -- Do the dragging
+    if index == 1 and selected ~= -1 then
         local groupX, groupY = windows[selected]["Group"].getPosition()
 
         local deltaX = x - lastPosX
@@ -107,15 +140,51 @@ while true do
 
         lastPosX = x
         lastPosY = y
+
+        return
+    end
+
+    retransmitMouseEvent("wm_glasses_drag", index, x, y)
+end
+
+
+-- Event handler, put into a program so "return" would work as "continue"
+function handleEvents()
+    local name, index, x, y = os.pullEvent()
+
+    -- Mouse events
+    if name == "glasses_click" then
+        handleGlassesClick(index, x, y)
+        return
+    end
+
+    if name == "glasses_up" then
+        handleGlassesUp(index, x, y)
+        return
+    end
+
+    if name == "glasses_drag" then
+        handleGlassesDrag(index, x, y)
+        return
+    end
+
+    if name == "glasses_scroll" then
+        retransmitMouseEvent("wm_glasses_scroll", index, x, y)
+        return
     end
 end    
+
+-- Main event loop
+while true do handleEvents() end
 
 
 -- A window is a table with the following values:
 --   Group - the window group itself
+--   ProgramGroup - group to which the program can draw
 --   Window - reference to the background rectangle, which is used to display the window's size (and for input events)
 --   Titlebar - reference to the window's titlebar
---   ButtonClose - reference to the close button
+--   Title - reference to the window's title
+--   Close - reference to the close button
 --
 -- On a glasses_click event, the window manager goes over every window group's Window, and checks if the input event happened within it
 --   If it did, it checks if it happened within that window's Titlebar
@@ -125,8 +194,15 @@ end
 --     If it did not, it sends the event and all the glasses_* events after it to the program that window belongs to, until a glasses_up event is received (event positions are adjusted to be relative to the window)
 --   If it did not, the event is ignored
 --
--- Window manager events:
--- program_terminate id - sent by the window manager when a window is closed, index is the program's associated with that window ID
--- window_close id - sent by the program to window manager, requesting to close the window with ID
--- glasses_down, glasses_drag, glasses_up, glasses_scroll, key, key_up - retransmitted to the program of the active window if did not happen on the titlebar or close button
+-- Events:
+--   Events sent by the program to the WM:
+--     program_create id sizeX sizeY title - request to create a new window with given id, size and title
+--     program_close id - request to close the window with ID
+--     program_resize id sizeX sizeY - request to resize the window with given id to size
+--   Events sent by the WM to the programs:
+--     wm_created id group - sent when a window is created, sends the group that the window can draw to
+--     wm_terminate id - sent when a window is closed, requesting program to close
+--     wm_glasses_down, wm_glasses_drag, wm_glasses_up, wm_glasses_scroll, wm_key, wm_key_up - 
+--                                    retransmitted to the program of the active window if did not happen on the titlebar or close button. 
+--                                    Same arguments as the regular function, but the first one is id of window it is directed at
 
